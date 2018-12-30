a177f94d6ec98da920e6661246ee16cceec8b053
smeikx <gthb@smeikx.com>
2018-12-30 16:38:48 +0100

    benenne Work zu Showcase um

---
+++ Assets/Showcase/Showcase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Showcase : MonoBehaviourWithGameManager
+{
+	void Start()
+	{
+		SetGameManager();
+	}
+
+	void Update()
+	{
+		transform.rotation *= Quaternion.Euler(GM.GetRelativeRotationInput().x, 0f, 0f);
+	}
+
+	public void Activate()
+	{
+	}
+}
