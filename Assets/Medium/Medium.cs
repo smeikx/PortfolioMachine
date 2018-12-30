268aca7ee68231e5bc74d7ba88bee91aed892165
smeikx <gthb@smeikx.com>
2018-12-30 19:46:22 +0100

    blende Medium außerhalb des Sichtfeldes aus
    Die Sichtbarkeit der Renderer-Komponente eines Mediums (Bild, Video,
    Audio) wird erst auf sichtbar gestellt, sobald sie sich im
    Kamera-Frustum befindet.
    
    Zur Erkennung werden die Bounds des Box-Colliders anstelle der des Renderers verwendet, weil
    für zukünftige Funktionalität ein Collider erforderlich sein wird.

---
+++ Assets/Medium/Medium.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Medium : MonoBehaviour
+{
+	Plane[] camPlanes;
+	Collider collider;
+	Renderer renderer;
+
+	void Start()
+	{
+		camPlanes = GeometryUtility.CalculateFrustumPlanes(Camera.main);
+		collider = GetComponent<BoxCollider>();
+		renderer = GetComponent<Renderer>();
+	}
+
+	void Update()
+	{
+		renderer.enabled = GeometryUtility.TestPlanesAABB(camPlanes, collider.bounds);
+	}
+}
